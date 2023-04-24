06baaec6a9c4d036b98190e815110c9d737ef0b8
Justin Anzalone <justin.m.anzalone@gmail.com>
2023-04-24 11:37:08 -0700

    Finished Exercises 1-5
    Homework assigned by Joe completed.

--- exercises/level1.js
+++ exercises/level1.js
@@ -1,34 +1,44 @@
 function lvl1exercise1 () {
   // Declare a variable as null and return it!
-
+  const variableNull = null;
+  return variableNull;
 }
 
 function lvl1exercise2 () {
   // Declare a variable with a number value and return it
-
+  const number = 12;
+  return number;
 }
 
 function lvl1exercise3 () {
   // Declare a variable with a floating point number value that is not a whole number and return it
-
+  const pi = 3.14;
+  return pi;
 }
 
 function lvl1exercise4 () {
   // Declare a variable and with a string value as "Hello World!" and return it
-
+  const hw = "Hello World!"
+  return hw;
 }
 
 function lvl1exercise5 () {
   // Declare a variable and with an array value containing the string "Hello World!" and the number 4 and return it
-
+  const arr1 = ["Hello World!", 4];
+  return arr1;
 }
 
 function lvl1exercise6 () {
   // Declare a variable as an object containing the key-value pairs key1 -> "Hello World!" and key2 -> 4, and return it
-
+  const obj1 = {
+    key1: "Hello World!",
+    key2: 4
+  }
+  return obj1;
 }
 
 function lvl1exercise7 () {
   // Declare a variable as a boolean value 'false' and return it
-
+  const bool1 = false;
+  return bool1;
 }
--- exercises/level2.js
+++ exercises/level2.js
@@ -1,24 +1,29 @@
 function lvl2exercise1 (num1, num2) {
   // Return the sum of num1 and num2
-
+  const sum = num1 + num2;
+  return sum;
 }
 
 function lvl2exercise2 (num1, num2) {
   // Return the difference of num1 and num2
-
+  const diff = num1 - num2;
+  return diff;
 }
 
 function lvl2exercise3 (num1, num2) {
   // Return the multiplication of num1 and num 2
-
+  const multi = num1 * num2;
+  return multi;
 }
 
 function lvl2exercise4 (num1, num2) {
   // Return the division of num1 and num2
-
+  const divide = num1 / num2;
+  return divide;
 }
 
 function lvl2exercise5 (num1) {
   // Add 2 to num1 using += and return it
-
+  const num2 = num1+=2;
+  return num2;
 }
